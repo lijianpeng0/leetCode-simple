4eb99d9e6d80f0c281413ea94383ba71a5c0b7c1
李建鹏 <544575791@qq.com>
2022-01-17 23:53:37 +0800

    合并两个有序链表

--- 合并两个有序链表.js
+++ 合并两个有序链表.js
@@ -10,44 +10,36 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-// const mergeTwoLists = function (l1, l2) {
-//   // const arr = [...l1]
-//   const arr = Array.from(l1)
-//   for (let i = 0; i < l2.length; i++) {
-
-//     arr.push(l2[i])
-//   }
-
-//   return arr.sort((a, b) => { return a - b })
-// }
-
 
 const mergeTwoLists = function (l1, l2) {
-  if (!l1 && !l2) return null
-  if (!l1 && l2) return l2
-  if (l1 && !l2) return l1
-  let p = l1, q = l2, head = null, l3 = null
-  while (p && q) {
-    let t = p.val <= q.val ? p : q
-    const node = new ListNode(t.val)
-    if (!head) {
-      head = node
-      l3 = node
+  let p1 = l1;
+  let p2 = l2;
+  let head = new ListNode(null); // 创建头节点 并用作指针
+  let p3 = head; // 用于保存合并的链表 头结点为 null 需要返回 p3.next
+
+  // 遍历链表，当有一个链表为空时跳出循环
+  while (p1 && p2) {
+    // 判断当前循环中值较小的节点
+    if (p1.val <= p2.val) {
+      // 头节点指向较小的节点
+      head.next = p1;
+      // 头结点后移一位
+      head = head.next;
+      // 较小节点的列表后移一位
+      p1 = p1.next;
     } else {
-      head.next = node
-      head = head.next
+      // 头节点指向较小的节点
+      head.next = p2;
+      // 头结点后移一位
+      head = head.next;
+      // 较小节点的列表后移一位
+      p2 = p2.next;
     }
-    if (t === p) {
-      p = p.next
-    } else {
-      q = q.next
-    }
   }
-  if (p) {
-    head.next = p
-  }
-  if (q) {
-    head.next = q
-  }
-  return l3
+  // 判断结束的链表是哪一个 结束的链表当前为null
+  // p1 结束 链接到 p2
+  // p2 结束 链接到 p1
+  head.next = p1 ? p1 : p2;
+
+  return p3.next;
 };
